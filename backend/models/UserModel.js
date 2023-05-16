55383bc40bb99f14b8402d05bde2ed0317d839e5
Uriadutu <balisoan03@gmail.com>
2023-05-16 07:34:44 +0700

    first commit

---
+++ backend/models/UserModel.js
@@ -0,0 +1,17 @@
+import {Sequelize} from 'sequelize';
+import db from '../config/Database.js';
+
+const { DataTypes } = Sequelize;
+
+const User = db.define('User', {
+    name : DataTypes.STRING,
+    email : DataTypes.STRING
+}, {
+    freezeTableName : true
+});
+
+export default User;
+
+(async () => {
+  await db.sync();
+})();
